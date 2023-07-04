7afe3d94bcd3b557c2ba55da84b24205ac8d1eea
Alobam776 <adekanbisamuel20023@gmail.com>
2023-07-04 18:35:04 +0100

    Todo app using React-typescript

---
+++ src/Components/TaskItem.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "../App.css";
+
+interface TaskProps {
+  id: number;
+  title: string;
+  completed: boolean;
+  onDelete: (id: number) => void;
+  onToggle: (id: number) => void;
+}
+
+const TaskItem: React.FC<TaskProps> = ({
+  id,
+  title,
+  completed,
+  onDelete,
+  onToggle,
+}) => {
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  const handleToggle = () => {
+    onToggle(id);
+  };
+
+  return (
+    <div className={`task ${completed ? "completed" : ""}`}>
+      <input type="checkbox" checked={completed} onChange={handleToggle} />
+
+      <span>{title}</span>
+      <button className="delete" onClick={handleDelete}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default TaskItem;
